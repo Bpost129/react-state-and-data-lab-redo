0beac7d1db03265abc87da02f4a15f6412aae9f9
Bpost129 <Bpost129@yahoo.com>
2024-11-30 16:30:36 -0500

    render temp user data to users page

--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -8,6 +8,9 @@ const Users = () => {
       <h1>User List</h1>
       <div className="card-container">
         {/* map the userData to UserCard components here */}
+        {userData.map(user => 
+          <div key={user.id}>{user.username}</div>
+        )}
       </div> 
     </>
   )

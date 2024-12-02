b383eb252b1d43c6f5d517293268d9cdb4c4d1dd
Bpost129 <Bpost129@yahoo.com>
2024-12-01 23:59:38 -0500

    update profile info on user cards

--- src/components/UserCard/UserCard.jsx
+++ src/components/UserCard/UserCard.jsx
@@ -4,8 +4,13 @@ const UserCard = ({ user }) => {
 
   return ( 
     <div className='user-card'>
+      {/* profile info */}
       <img src={user.avatar} alt="user avatar" />
       <div>Name: {user.name}</div>
+      <div>Username: {user.username}</div>
+
+      {/* contact info */}
+      {/* <div>Address: {user.address}</div> */}
       <div>Company: {user.company.name}</div>
       <div>Contact: {user.phone}</div>
     </div>
